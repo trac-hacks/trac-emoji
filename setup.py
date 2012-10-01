5cc31260146011bc67d058ce63f1a02c29bbee54
Hatem Nassrat <hnassrat@gmail.com>
2012-10-01 06:59:34 -0300

    Adding user info for upload.

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ version='0.1'
 setup(
     name='TracEmoji',
     long_description='Emoji plugin for trac implementing https://github.com/arvida/emoji-cheat-sheet.com',
+    author='Hatem Nassrat',
+    author_email='hatem@nassrat.ca',
     version=version,
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
